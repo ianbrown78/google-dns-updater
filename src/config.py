d425e1e4ff378f1b94f72ade0f8b9e11aebc883e
Ian Brown <ian@carelynx.com>
2020-01-31 13:02:11 +1000

    Making this shit work.

--- src/config.py
+++ src/config.py
@@ -1,12 +1,17 @@
 import logging
 import os
-import string
-from distutils import util
+from dotenv import load_dotenv
+from pathlib import Path
 
 logger = logging.getLogger('root')
 
 class config():
+  env_path = Path('.') / '.env'
+  load_dotenv(dotenv_path=env_path)
+
   def __init__(self):
+    self.ttl = os.getenv("ttl", 3600)
+    
     self.app = os.getenv("app", "")
 
     self.gcpProject = os.getenv("project", "")
@@ -14,4 +19,4 @@ class config():
     self.gcpDnsZoneName = os.getenv("dnsZoneName", "")
     self.gcpDnsDomain = os.getenv("dnsDomain", "")
     
-cfg = config() 
+cfg = config()
--- src/main.py
+++ src/main.py
@@ -1,27 +1,129 @@
 # The main python file that does the work
 from google.cloud import dns
+from google.oauth2 import service_account
 import config
 import time
+import sys, urllib
+import flask
+from flask import request, jsonify
 
-def main():
-  cfg = config.cfg
-  client = dns.Client(project=cfg.gcpProject)
-  zone = client.zone(cfg.gcpZoneName, cfg.gcpDnsDomain)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
+# Grab our configuration
+cfg = config.cfg
+
+# Configure the client & zone
+if (len(cfg.gcpAuthKeyJsonFile) == 0):
+  credentials, project = google.auth.default()
+else:
+  credentials = service_account.Credentials.from_service_account_file(cfg.gcpAuthKeyJsonFile)
+client = dns.Client(project=cfg.gcpProject, credentials=credentials)
+zone = client.zone(cfg.gcpDnsZoneName, cfg.gcpDnsDomain)
+
+records = ""
+changes = zone.changes()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+@app.route('/', methods=['POST'])
+def home():
+  query_parameters = request.args
+  
+  # Assign our parameters
+  host = query_parameters.get('host')
+  ip = query_parameters.get('ip')
+  key = query_parameters.get('key')
+
+  # Check we have the required parameters
+  if not (host and ip and key):
+    return page_not_found(404)
+
+  # Check the key
+  if not (check_key(key)):
+    return page_not_found(404)
+
+  # Get a list of the current records
+  records = get_records()
+  
+  # Check for matching records
+  for record in records:
+    if (host == record.name):
+      for data in record.rrdatas:
+        if (test_for_record_change(data, ip)):
+          add_to_change_set(record, 'delete')
+          add_to_change_set(create_record_set(host, record.record_type, ip), 'create')
+          execute_change_set(changes)
+          return "Change successful."
+        else:
+          return "Record up to date."
+  
+  return "No matching records."
+
+def check_key(key):
+  if (cfg.app == key):
+    return True
+  else:
+    return False
+
+def get_records(client=client, zone=zone):
   # Get the records in batches
-  records, page_token = zone.list_resource_record_sets()
-  while page_token is not None:
-    next_batch, page_token = zone.list_resource_record_sets(
-      page_token=page_token)  # API request
-    records.extend(next_batch)
+  return zone.list_resource_record_sets(max_results=100, page_token=None, client=client)
 
-  # create an update
+def test_for_record_change(old_ip, new_ip):
+  if (old_ip != new_ip):
+    return True
+  else:
+    return False
+
+def create_record_set(host, record_type, ip):
   record_set = zone.resource_record_set(
-    'www.example.com.', 'CNAME', TWO_HOURS, ['www1.example.com.',])
-  changes = zone.changes()
-  changes.add_record_set(record_set)
-  changes.create()  # API request
+    host, record_type, cfg.ttl, [ip])
+  return record_set  
+
+def add_to_change_set(record_set, atype):
+  if (atype == 'delete'):
+    return changes.delete_record_set(record_set)
+  else:
+    return changes.add_record_set(record_set)
+
+def execute_change_set(changes):
+  changes.create()
   while changes.status != 'done':
     print('Waiting for changes to complete')
-    time.sleep(60)     # or whatever interval is appropriate
-    changes.reload()   # API request
+    time.sleep(20)
+    changes.reload()
+
+app.run()
+
+# def main():
+#   # Get the query params
+#   query_string = sys.stdin.read()
+#   multiform = urllib.parse.parse_qs(query_string)
+
+#   # Grab our configuration
+#   cfg = config.cfg
+
+#   # Setup the client and zone
+#   client = dns.Client(project=cfg.gcpProject)
+#   zone = client.zone(cfg.gcpZoneName, cfg.gcpDnsDomain)
+
+#   # Get the records in batches
+#   records, page_token = zone.list_resource_record_sets()
+#   while page_token is not None:
+#     next_batch, page_token = zone.list_resource_record_sets(
+#       page_token=page_token)  # API request
+#     records.extend(next_batch)
+
+#   # create an update
+#   record_set = zone.resource_record_set(
+#     param['host'], 'A', cfg.ttl, [param['ip']])
+#   changes = zone.changes()
+#   changes.add_record_set(record_set)
+#   changes.create()  # API request
+#   while changes.status != 'done':
+#     print('Waiting for changes to complete')
+#     time.sleep(20)     # or whatever interval is appropriate
+#     changes.reload()   # API request
