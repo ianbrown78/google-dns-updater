2c8d42d02bd2d20bed1b9bafb63b5600b93cf303
Ian Brown <ian@carelynx.com>
2020-01-31 14:03:11 +1000

    Make sure we check that the .env file exists before attempting to use it.

--- config.py
+++ config.py
@@ -6,8 +6,14 @@ from pathlib import Path
 logger = logging.getLogger('root')
 
 class config():
-  env_path = Path('.') / '.env'
-  load_dotenv(dotenv_path=env_path)
+  try:
+    f = open(".env")
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+  except IOError:
+    print("File not accessible")
+  finally:
+    f.close()
 
   def __init__(self):
     self.ttl = os.getenv("ttl", 3600)
