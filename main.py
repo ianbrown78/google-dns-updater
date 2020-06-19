caf66f159765d3b58b0c4377d8f7e8aff417b6b2
Ian Brown <ianbrown78@gmail.com>
2020-06-19 14:46:00 +1000

    Add more logging to the application
    Added more logging to the application so that we can see when there is a failure.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # The main python file that does the work
 from google.cloud import dns
+from google.cloud import logging
+from google.cloud.logging.resource import Resource
 from google.oauth2 import service_account
 import google.auth
 import config
@@ -19,6 +21,16 @@ if (len(cfg.gcpAuthKeyJsonFile) == 0):
   credentials, project = google.auth.default()
 else:
   credentials = service_account.Credentials.from_service_account_file(cfg.gcpAuthKeyJsonFile)
+  log_client = logging.Client()
+  log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions' 
+  res = Resource(type="cloud_function", 
+               labels={
+                   "function_name": cfg.function_name, 
+                   "region": cfg.region
+               },
+              )
+  logger = log_client.logger(log_name.format(cfg.project))
+
 client = dns.Client(project=cfg.gcpProject, credentials=credentials)
 zone = client.zone(cfg.gcpDnsZoneName, cfg.gcpDnsDomain)
 
@@ -26,18 +38,22 @@ records = ""
 changes = zone.changes()
 
 def page_not_found(e):
+    logger.log_struct({"message": "The resource could not be found."}, resource=res, severity='ERROR')
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 def page_unauthorized(e):
+    logger.log_struct({"message": "You are not authorized to access this resource."}, resource=res, severity='ERROR')
     return "<h1>401</h1><p>You are not authorized to access this resource.</p>", 401
 
 def main(request):
+  logger.log_struct({"message": "Update request started."}, resource=res, severity='INFO')
   query_parameters = request.args
   
   # Assign our parameters
   host = query_parameters.get('host')
   ip = query_parameters.get('ip')
   key = query_parameters.get('key')
+  logger.log_struct({"message": "IP to update to is: %s", ip}, resource=res, severity='INFO')
 
   # Check we have the required parameters
   if not (host and ip and key):
@@ -75,6 +91,7 @@ def get_records(client=client, zone=zone):
   return zone.list_resource_record_sets(max_results=100, page_token=None, client=client)
 
 def test_for_record_change(old_ip, new_ip):
+  logger.log_struct({"message": "Existing IP is: %s", old_ip}, resource=res, severity='INFO')
   if (old_ip != new_ip):
     return True
   else:
@@ -92,9 +109,10 @@ def add_to_change_set(record_set, atype):
     return changes.add_record_set(record_set)
 
 def execute_change_set(changes):
+  logger.log_struct({"message": "Change set executed"}, resource=res, severity='INFO')
   changes.create()
   while changes.status != 'done':
-    print('Waiting for changes to complete')
+    logger.log_struct({"message": "Waiting for changes to complete. Change status: %s", changes.status}, resource=res, severity='INFO')
     time.sleep(20)
     changes.reload()
 
