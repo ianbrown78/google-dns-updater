abd905dfe1a84490d7a9583d3b09268a5c492e8a
Ian Brown <ianbrown78@gmail.com>
2021-01-09 16:02:04 +1000

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,13 @@ import os.path
 from dotenv import load_dotenv
 from pathlib import Path
 
-logger = logging.getLogger('root')
-
 class config():
   if (os.path.isfile('.env')):
     env_path = Path('.') / '.env'
     load_dotenv(dotenv_path=env_path)
 
   def __init__(self):
+    logging.info("Starting configuration.")
     self.ttl = os.getenv("ttl", 3600)
     
     self.app = os.getenv("app", "")
